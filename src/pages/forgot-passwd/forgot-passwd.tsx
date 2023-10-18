9e7cca7372e4115459946a69df974d5197d059ee
yanglee2421 <yanglee2421@gmail.com>
2023-10-18 22:13:21 +0800

    i18n demo

--- src/pages/forgot-passwd/forgot-passwd.tsx
+++ src/pages/forgot-passwd/forgot-passwd.tsx
@@ -3,14 +3,22 @@ import { useTranslation } from "react-i18next";
 
 // Components Imports
 import { LangSelect } from "@/components";
-import { Typography } from "@mui/material";
+
+// MUI Imports
+import { Typography, Grid } from "@mui/material";
 
 export function ForgotPasswd() {
   const { t } = useTranslation();
   return (
     <>
-      <Typography>{t("hello")}</Typography>
-      <LangSelect />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Typography>{t("hello")}</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <LangSelect />
+        </Grid>
+      </Grid>
     </>
   );
 }
