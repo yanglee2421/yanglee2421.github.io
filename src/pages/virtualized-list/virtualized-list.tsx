352dd35f0e7998b85fa1bfe906d03115f57d3746
yanglee2421 <yanglee2421@gmail.com>
2023-10-22 00:10:44 +0800

    infinite list

--- src/pages/virtualized-list/virtualized-list.tsx
+++ src/pages/virtualized-list/virtualized-list.tsx
@@ -1,36 +1,74 @@
 // MUI Imports
-import { Box, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import {
+  Box,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Grid,
+} from "@mui/material";
 
 // Components Imports
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
+import InfiniteLoader from "react-window-infinite-loader";
+
+// React Imports
+import React from "react";
 
 export function VirtualizedList() {
+  const infiniteRef = React.useRef<InfiniteLoader>(null);
+
   return (
     <>
-      <Box
-        sx={{
-          width: 360,
-          height: 520,
-          bgcolor: "background.paper",
-        }}
-      >
-        <AutoSizer>
-          {(size) => {
-            return (
-              <FixedSizeList
-                height={size.height}
-                width={size.width}
-                itemSize={46}
-                itemCount={200}
-                overscanCount={5}
-              >
-                {renderRow}
-              </FixedSizeList>
-            );
-          }}
-        </AutoSizer>
-      </Box>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={6}>
+          <Box
+            sx={{
+              height: 420,
+              bgcolor: "background.paper",
+            }}
+          >
+            <AutoSizer>
+              {(size) => {
+                return (
+                  <FixedSizeList
+                    height={size.height}
+                    width={size.width}
+                    itemSize={46}
+                    itemCount={200}
+                    overscanCount={5}
+                  >
+                    {renderRow}
+                  </FixedSizeList>
+                );
+              }}
+            </AutoSizer>
+          </Box>
+        </Grid>
+        <Grid xs={12} md={6}>
+          <InfiniteLoader
+            ref={infiniteRef}
+            isItemLoaded={() => true}
+            itemCount={200}
+            loadMoreItems={() => {}}
+          >
+            {({ onItemsRendered, ref }) => {
+              return (
+                <FixedSizeList
+                  ref={ref}
+                  itemCount={200}
+                  onItemsRendered={onItemsRendered}
+                  height={520}
+                  width={400}
+                  itemSize={46}
+                >
+                  {renderRow}
+                </FixedSizeList>
+              );
+            }}
+          </InfiniteLoader>
+        </Grid>
+      </Grid>
     </>
   );
 }
