e6a49915d18cf069356e0ab11ebbc30e41d2b6cd
ZhangSheng <yanglee2421@gmail.com>
2023-10-24 12:02:54 +0800

    the margin of flex items

--- src/pages/home/card-radio.tsx
+++ src/pages/home/card-radio.tsx
@@ -53,9 +53,9 @@ export function CardRadio() {
                   mt={2}
                   fontWeight={500}
                   overflow={"hidden"}
-                  maxHeight={(theme) =>
-                    `calc(${theme.typography.body1.lineHeight}em * 1)`
-                  }
+                  maxHeight={(theme) => {
+                    return `calc(${theme.typography.body1.lineHeight}em * 1)`;
+                  }}
                 >
                   Visually Similar Recommendations
                 </Typography>
@@ -64,9 +64,9 @@ export function CardRadio() {
                   mt={1}
                   mb={"auto"}
                   overflow={"hidden"}
-                  maxHeight={(theme) =>
-                    `calc(${theme.typography.body2.lineHeight}em * 4)`
-                  }
+                  maxHeight={(theme) => {
+                    return `calc(${theme.typography.body2.lineHeight}em * 4)`;
+                  }}
                 >
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. At,
                   possimus itaque. Suscipit sequi, minus dignissimos reiciendis
@@ -96,9 +96,9 @@ export function CardRadio() {
                   mt={2}
                   fontWeight={500}
                   overflow={"hidden"}
-                  maxHeight={(theme) =>
-                    `calc(${theme.typography.body1.lineHeight}em * 1)`
-                  }
+                  maxHeight={(theme) => {
+                    return `calc(${theme.typography.body1.lineHeight}em * 1)`;
+                  }}
                 >
                   Collaborative Filtering Recommendations
                 </Typography>
@@ -107,12 +107,33 @@ export function CardRadio() {
                   mt={1}
                   mb={"auto"}
                   overflow={"hidden"}
-                  maxHeight={"calc(1.4em * 4)"}
+                  maxHeight={(theme) => {
+                    return `calc(${theme.typography.body2.lineHeight}em * 4)`;
+                  }}
                 >
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                   Tempore enim omnis nesciunt expedita earum nihil.
                 </Typography>
                 <Radio value={"two"} />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                height={"100%"}
+                border={(theme) => `1px solid ${theme.palette.divider}`}
+                textAlign={"center"}
+              >
+                <p>7891231</p>
+                <p
+                  style={{
+                    marginTop: "auto",
+                    marginBottom: "auto",
+                  }}
+                >
+                  typography
+                </p>
               </Box>
             </Grid>
           </Grid>
@@ -121,3 +142,8 @@ export function CardRadio() {
     </Card>
   );
 }
+/**
+ * With auto margins, flex items can be centered, spaced away or packed into sub-groups.
+ * Unlike justify-content, which is applied to the flex container, auto margins go on flex items.
+ * They work by consuming all free space in the specified direction.
+ */
