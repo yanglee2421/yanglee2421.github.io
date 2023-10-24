3b720bda5819d8220fa9e082ff64b182ac2418d8
yanglee2421 <yanglee2421@gmail.com>
2023-10-24 21:52:37 +0800

    mui theme

--- src/pages/home/card-radio.tsx
+++ src/pages/home/card-radio.tsx
@@ -30,52 +30,7 @@ export function CardRadio() {
       <CardContent>
         <RadioGroup value={value} onChange={handleChange}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-              <Box
-                component={"label"}
-                display={"flex"}
-                flexDirection={"column"}
-                alignItems={"center"}
-                height={"100%"}
-                p={2}
-                border={"1px solid"}
-                borderColor={(theme) =>
-                  true ? theme.palette.primary.main : theme.palette.divider
-                }
-                borderRadius={1}
-                textAlign={"center"}
-                sx={{
-                  cursor: "pointer",
-                }}
-              >
-                <Avatar sx={{ width: 64, height: 64 }}></Avatar>
-                <Typography
-                  mt={2}
-                  fontWeight={500}
-                  overflow={"hidden"}
-                  maxHeight={(theme) => {
-                    return `calc(${theme.typography.body1.lineHeight}em * 1)`;
-                  }}
-                >
-                  Visually Similar Recommendations
-                </Typography>
-                <Typography
-                  variant="body2"
-                  mt={1}
-                  mb={"auto"}
-                  overflow={"hidden"}
-                  maxHeight={(theme) => {
-                    return `calc(${theme.typography.body2.lineHeight}em * 4)`;
-                  }}
-                >
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. At,
-                  possimus itaque. Suscipit sequi, minus dignissimos reiciendis
-                  cumque rem voluptatem nesciunt, iure, in ipsam quasi quod a.
-                  Adipisci alias quibusdam eveniet.
-                </Typography>
-                <Radio value={"one"} />
-              </Box>
-            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}></Grid>
             <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
               <Box
                 component={"label"}
--- src/themes/theme-utils.ts
+++ src/themes/theme-utils.ts
@@ -1,19 +1,90 @@
 // MUI Imports
 import { createTheme } from "@mui/material";
-import { red, blue } from "@mui/material/colors";
 
 export function toThemeValue(params: ToThemeValueParams) {
   // ** Params
   const { isDark } = params;
 
+  const lightColor = "58, 53, 65";
+  const darkColor = "231, 227, 252";
+  const mainColor = isDark ? darkColor : lightColor;
+
   return createTheme({
     palette: {
       mode: isDark ? "dark" : "light",
+      common: {
+        black: "#000",
+        white: "#fff",
+      },
       primary: {
-        main: blue[500],
+        main: "#9155FD",
+        light: "#9E69FD",
+        dark: "#804BDF",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#8A8D93",
+        light: "#9C9FA4",
+        dark: "#777B82",
+        contrastText: "#fff",
+      },
+      success: {
+        light: "#6AD01F",
+        main: "#56CA00",
+        dark: "#4CB200",
+        contrastText: "#fff",
       },
       error: {
-        main: red[500],
+        main: "#FF4C51",
+        light: "#FF6166",
+        dark: "#E04347",
+        contrastText: "#fff",
+      },
+      warning: {
+        light: "#FFCA64",
+        main: "#FFB400",
+        dark: "#E09E00",
+        contrastText: "#fff",
+      },
+      info: {
+        light: "#32BAFF",
+        main: "#16B1FF",
+        dark: "#139CE0",
+        contrastText: "#fff",
+      },
+      grey: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#EEEEEE",
+        300: "#E0E0E0",
+        400: "#BDBDBD",
+        500: "#9E9E9E",
+        600: "#757575",
+        700: "#616161",
+        800: "#424242",
+        900: "#212121",
+        A100: "#F5F5F5",
+        A200: "#EEEEEE",
+        A400: "#BDBDBD",
+        A700: "#616161",
+      },
+      text: {
+        primary: `rgba(${mainColor}, 0.87)`,
+        secondary: `rgba(${mainColor}, 0.6)`,
+        disabled: `rgba(${mainColor}, 0.38)`,
+      },
+      divider: `rgba(${mainColor}, 0.12)`,
+      background: {
+        paper: isDark ? lightColor : "#fff",
+        default: "#F4F5FA",
+      },
+      action: {
+        active: `rgba(${mainColor}, 0.54)`,
+        hover: `rgba(${mainColor}, 0.04)`,
+        selected: `rgba(${mainColor}, 0.08)`,
+        disabled: `rgba(${mainColor}, 0.26)`,
+        disabledBackground: `rgba(${mainColor}, 0.12)`,
+        focus: `rgba(${mainColor}, 0.12)`,
       },
     },
     typography(palette) {
