5408e50ec03691074b11ccbfb1c4c4015fda4fb5
yanglee2421 <yanglee2421@gmail.com>
2023-10-31 00:44:59 +0800

    page 404

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,14 +2,52 @@
 import { NavLink } from "react-router-dom";
 
 // MUI Imports
-import { Button } from "@mui/material";
+import { Box, Button, Typography, Container } from "@mui/material";
+
+// Assets Imports
+import snowVillage from "@/assets/images/snow-village.jpg";
 
 export function Component() {
   return (
-    <NavLink to="/">
-      <Button variant="contained" size="large">
-        Take me home
-      </Button>
-    </NavLink>
+    <Box
+      position={"fixed"}
+      display={"flex"}
+      sx={{
+        inset: 0,
+        backgroundImage: `url(${snowVillage})`,
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        // backgroundAttachment: "fixed",
+      }}
+    >
+      <Box
+        position={"absolute"}
+        sx={{
+          inset: 0,
+          zIndex: -1,
+          backgroundColor: "rgba(0,0,0,.15)",
+          backdropFilter: "blur(10px)",
+        }}
+      ></Box>
+      <Container disableGutters sx={{ pt: 36, px: 16 }}>
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h2">Page not found</Typography>
+        <Typography mt={6}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur eum
+          nulla possimus officiis, placeat ut minima quo blanditiis maxime aut
+          eius tenetur assumenda recusandae, laudantium quae ex. Voluptates, qui
+          iusto!
+        </Typography>
+        <Button
+          component={NavLink}
+          to="/"
+          variant="contained"
+          size="large"
+          sx={{ mt: 6, borderRadius: 50 }}
+        >
+          Take me home
+        </Button>
+      </Container>
+    </Box>
   );
 }
