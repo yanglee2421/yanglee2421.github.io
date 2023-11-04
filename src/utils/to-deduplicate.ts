8d0d9053f1bc666c269d81c317280a275596e351
yanglee <yanglee2421@gmail.com>
2023-11-04 19:11:27 +0800

    update toDeduplicate

--- src/utils/to-deduplicate.ts
+++ src/utils/to-deduplicate.ts
@@ -1,16 +1,33 @@
 export function toDeduplicate(items: unknown[], ops: Partial<Ops> = {}) {
-  const { isCover = false, keyProp = "id" } = ops;
+  const { overwrite = false, key = "id" } = ops;
 
   const map = new Map();
 
   items.forEach((item) => {
-    if (!item) throw new Error("invalid item");
-    if (typeof item !== "object") throw new Error("Item must be an object!");
+    // Item must be an object
+    if (typeof item !== "object") {
+      console.error("Excepted an object");
+      return;
+    }
 
-    const key = Reflect.get(item, keyProp);
-    if (!key) throw new Error("invalid key");
+    if (!item) {
+      console.error("Excepted an object, got a falsy!");
+      return;
+    }
 
-    if (isCover) return map.set(key, item);
+    // Get Key
+    const keyValue = Reflect.get(item, key);
+    if (!keyValue) {
+      console.error("Excepted a truth, got a falsy!");
+      return;
+    }
+
+    // Whether to allow overwriting
+    if (overwrite) {
+      map.set(key, item);
+      return;
+    }
+
     map.get(key) ?? map.set(key, item);
   });
 
@@ -18,6 +35,6 @@ export function toDeduplicate(items: unknown[], ops: Partial<Ops> = {}) {
 }
 
 interface Ops {
-  isCover: boolean;
-  keyProp: string | symbol;
+  key: string | symbol;
+  overwrite: boolean;
 }
