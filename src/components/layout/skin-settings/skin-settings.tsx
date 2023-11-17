9cc12d0e1cca94530f1035f66445dbcfdcf22ec5
yanglee2421 <yanglee2421@gmail.com>
2023-11-17 12:46:14 +0800

    Working on skin-settings

--- src/components/layout/skin-settings/skin-settings.tsx
+++ src/components/layout/skin-settings/skin-settings.tsx
@@ -8,6 +8,7 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
+  Divider,
 } from "@mui/material";
 import { AutoAwesomeOutlined, CloseOutlined } from "@mui/icons-material";
 
@@ -40,6 +41,8 @@ export function SkinSettings(props: IconButtonProps) {
         open={open}
         onClose={handleClose}
         anchor="right"
+        variant="persistent"
+        hideBackdrop
         sx={{
           "& .MuiDrawer-paper": {
             width: "100%",
@@ -62,7 +65,13 @@ export function SkinSettings(props: IconButtonProps) {
             borderColor={(theme) => theme.palette.divider}
           >
             <Box>
-              <Typography variant="h6">THEME CUSTOMIZER</Typography>
+              <Typography
+                variant="h6"
+                fontWeight={600}
+                textTransform={"uppercase"}
+              >
+                THEME CUSTOMIZER
+              </Typography>
               <Typography color="secondary">
                 Customize & Preview in Real Time
               </Typography>
@@ -76,29 +85,78 @@ export function SkinSettings(props: IconButtonProps) {
               <Box height={1000} p={4}>
                 <Typography
                   variant="caption"
+                  component={"p"}
+                  mb={4}
                   color={(theme) => theme.palette.text.disabled}
+                  textTransform={"uppercase"}
                 >
                   THEMING
                 </Typography>
-                <Typography>Skin</Typography>
-                <RadioGroup
-                  value={"default"}
-                  onChange={(evt, v) => {
-                    console.log(evt, v);
-                  }}
-                  row
-                >
-                  <FormControlLabel
-                    control={<Radio />}
-                    label={<Typography variant="body2">Default</Typography>}
+
+                {/* Skin */}
+                <Box mb={4}>
+                  <Typography>Skin</Typography>
+                  <RadioGroup
                     value={"default"}
-                  />
-                  <FormControlLabel
-                    control={<Radio />}
-                    label={<Typography variant="body2">Bordered</Typography>}
-                    value={"bordered"}
-                  />
-                </RadioGroup>
+                    onChange={(evt, v) => {
+                      console.log(evt, v);
+                    }}
+                    row
+                  >
+                    <FormControlLabel
+                      control={<Radio />}
+                      label={<Typography variant="body2">Default</Typography>}
+                      value={"default"}
+                    />
+                    <FormControlLabel
+                      control={<Radio />}
+                      label={<Typography variant="body2">Bordered</Typography>}
+                      value={"bordered"}
+                    />
+                  </RadioGroup>
+                </Box>
+
+                {/* Mode */}
+                <Box mb={4}>
+                  <Typography>Mode</Typography>
+                  <RadioGroup
+                    value={"default"}
+                    onChange={(evt, v) => {
+                      console.log(evt, v);
+                    }}
+                    row
+                  >
+                    <FormControlLabel
+                      control={<Radio />}
+                      label={<Typography variant="body2">Light</Typography>}
+                      value={"light"}
+                    />
+                    <FormControlLabel
+                      control={<Radio />}
+                      label={<Typography variant="body2">Dark</Typography>}
+                      value={"dark"}
+                    />
+                    <FormControlLabel
+                      control={<Radio />}
+                      label={<Typography variant="body2">Auto</Typography>}
+                      value={"auto"}
+                    />
+                  </RadioGroup>
+                </Box>
+
+                {/*  */}
+                <Divider sx={{ m: 0 }}></Divider>
+
+                {/* Layout */}
+                <Typography
+                  variant="caption"
+                  component={"p"}
+                  mb={4}
+                  color={(theme) => theme.palette.text.disabled}
+                  textTransform={"uppercase"}
+                >
+                  layout
+                </Typography>
               </Box>
             </Scrollbar>
           </Box>
