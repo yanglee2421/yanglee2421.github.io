8ccb8a05f68849fe5829ccbe9b3708e8cc285880
ZhangSheng <yanglee2421@gmail.com>
2024-01-20 10:13:01 +0800

    feat: add swiper

--- src/pages/transition/Transition.tsx
+++ src/pages/transition/Transition.tsx
@@ -4,14 +4,14 @@ import { Stack } from "@mui/material";
 // Components Imports
 import { FadeButton } from "./FadeButton";
 import { FadeCard } from "./FadeCard";
-import { Swiper } from "./Swiper";
+import { SwiperCard } from "./SwiperCard";
 
 export function Transition() {
   return (
     <Stack spacing={6}>
       <FadeButton></FadeButton>
       <FadeCard></FadeCard>
-      <Swiper></Swiper>
+      <SwiperCard></SwiperCard>
     </Stack>
   );
 }
