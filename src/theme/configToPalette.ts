d7d1a717247cbd9e0ff2a331754767ee41705b95
ZhangSheng <yanglee2421@gmail.com>
2024-02-04 10:36:31 +0800

    style: theme palette

--- src/theme/configToPalette.ts
+++ src/theme/configToPalette.ts
@@ -7,12 +7,39 @@ export function configToPalette(params: PaletteConfig): PaletteOptions {
 
   return {
     mode,
+    primary: {
+      main: "#6777ef",
+      dark: "#4853a7",
+      light: "#8592f2",
+    },
+    secondary: {
+      main: "#909399",
+      dark: "#64666b",
+      light: "#a6a8ad",
+    },
+    success: {
+      main: "#52c41a",
+      dark: "#398912",
+      light: "#74cf47",
+    },
+    error: {
+      main: "#ff4d4f",
+      dark: "#b23537",
+      light: "#ff7072",
+    },
+    warning: {
+      main: "#ffc107",
+      dark: "#ffa426",
+      light: "#ffc473",
+    },
+    info: {
+      main: "#1677ff",
+      dark: "#0f53b2",
+      light: "#4492ff",
+    },
   };
 }
 
 interface PaletteConfig {
   mode: "dark" | "light";
-  lightColor: string;
-  darkColor: string;
-  whiteColor: string;
 }
