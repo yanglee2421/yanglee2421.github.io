b8feb57dfb62bb14a85011bb1432837595ed3e73
yanglee <yanglee2421@gmail.com>
2024-02-13 21:00:31 +0800

    feat: add swiper scrollbar feature

--- src/pages/home/HomeSwiper.tsx
+++ src/pages/home/HomeSwiper.tsx
@@ -1,27 +1,40 @@
 // MUI Imports
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import {
+  NavigateNextOutlined,
+  NavigateBeforeOutlined,
+} from "@mui/icons-material";
 
 // Utils Imports
 import Swiper from "swiper";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Navigation, Pagination, Autoplay, Scrollbar } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 // React Imports
 import React from "react";
+import { useImmer } from "use-immer";
 
 export function HomeSwiper() {
   const swiperRef = React.useRef<HTMLDivElement>(null);
-  const prevElRef = React.useRef<HTMLDivElement>(null);
-  const nextElRef = React.useRef<HTMLDivElement>(null);
+  const prevElRef = React.useRef<HTMLButtonElement>(null);
+  const nextElRef = React.useRef<HTMLButtonElement>(null);
   const paginationElRef = React.useRef<HTMLDivElement>(null);
+  const scrollbarElRef = React.useRef<HTMLDivElement>(null);
+
+  const [state, updateState] = useImmer({
+    isBeginning: false,
+    isEnd: false,
+  });
 
   React.useEffect(() => {
     const swiperEl = swiperRef.current;
     const prevEl = prevElRef.current;
     const nextEl = nextElRef.current;
     const paginationEl = paginationElRef.current;
+    const scrollbarEl = scrollbarElRef.current;
 
     if (!swiperEl) {
       return;
@@ -39,8 +52,12 @@ export function HomeSwiper() {
       return;
     }
 
+    if (!scrollbarEl) {
+      return;
+    }
+
     const swiper = new Swiper(swiperEl, {
-      modules: [Navigation, Pagination, Autoplay],
+      modules: [Navigation, Pagination, Autoplay, Scrollbar],
       navigation: {
         prevEl,
         nextEl,
@@ -51,12 +68,34 @@ export function HomeSwiper() {
       autoplay: {
         delay: 1000 * 5,
       },
+      scrollbar: {
+        el: scrollbarEl,
+      },
+    });
+    updateState((state) => {
+      state.isBeginning = swiper.isBeginning;
+      state.isEnd = swiper.isEnd;
+    });
+
+    swiper.on("slideChange", (swiper) => {
+      console.log(swiper);
+
+      updateState((state) => {
+        state.isBeginning = swiper.isBeginning;
+        state.isEnd = swiper.isEnd;
+      });
+    });
+    swiper.on("scroll", (swiper) => {
+      updateState((state) => {
+        state.isBeginning = swiper.isBeginning;
+        state.isEnd = swiper.isEnd;
+      });
     });
 
     return () => {
       swiper.destroy();
     };
-  }, []);
+  }, [updateState]);
 
   return (
     <Box
@@ -76,16 +115,44 @@ export function HomeSwiper() {
         },
       }}
     >
+      <Box
+        ref={scrollbarElRef}
+        className="swiper-scrollbar"
+        sx={{ zIndex: 2, position: "relative" }}
+      ></Box>
       <Box className="swiper-wrapper">
         <Box className="swiper-slide">Slide 1</Box>
         <Box className="swiper-slide">Slide 2</Box>
         <Box className="swiper-slide">Slide 3</Box>
       </Box>
 
-      <Box className="swiper-button-prev" ref={prevElRef}></Box>
-      <Box className="swiper-button-next" ref={nextElRef}></Box>
+      <IconButton
+        ref={prevElRef}
+        disabled={state.isBeginning}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: 0,
+          zIndex: 1,
+          transform: "translateY(-50%)",
+        }}
+      >
+        <NavigateBeforeOutlined></NavigateBeforeOutlined>
+      </IconButton>
+      <IconButton
+        ref={nextElRef}
+        disabled={state.isEnd}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: 0,
+          zIndex: 1,
+          transform: "translateY(-50%)",
+        }}
+      >
+        <NavigateNextOutlined></NavigateNextOutlined>
+      </IconButton>
       <Box className="swiper-pagination" ref={paginationElRef}></Box>
-      <Box className="swiper-scrollbar"></Box>
     </Box>
   );
 }
