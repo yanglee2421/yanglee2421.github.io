a690ad008d70078c4d3086197ebadd80d9d039cb
yanglee2421 <yanglee2421@gmail.com>
2024-08-05 16:51:48 +0800

    chore: working on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,25 +1,18 @@
 import {
-  AddOutlined,
-  AddReactionOutlined,
-  SendOutlined,
-} from "@mui/icons-material";
-import {
   Box,
-  Divider,
   Grid,
-  InputBase,
   Paper,
   Stack,
   alpha,
-  IconButton,
-  styled,
   Typography,
   Link,
+  Card,
+  CardContent,
+  CardHeader,
 } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import React from "react";
 import bgImg from "@/assets/images/justHer.jpg";
-import { ScrollView } from "@/components/ui/ScrollView";
 import { AsyncStore } from "./AyncStore";
 import { Calendar } from "./Calendar";
 import { Countdown } from "./Countdown";
@@ -30,10 +23,7 @@ import { TipTap } from "./TipTap";
 export function Home() {
   const [number, setNumber] = React.useState(Number.NaN);
 
-  const ref = React.useRef<HTMLElement>(null);
-
   const [html, setHtml] = React.useState("<p>hello world</p>");
-  const [chatInput, setChatInput] = React.useState("");
 
   return (
     <Grid container spacing={{ xs: 3, md: 4 }}>
@@ -123,122 +113,23 @@ export function Home() {
         </Paper>
       </Grid>
       <Grid item xs={12}>
-        <Paper>
-          {(() => {
-            const headerText = 'attachment;  filename="filename.jpg"';
-
-            const reg = /^attachment;( *)filename="(?<filename>.+)"$/;
-
-            return reg.exec(headerText)?.groups?.filename;
-          })()}
-        </Paper>
+        <Card>
+          <CardHeader title="read only" />
+          <CardContent>
+            <TipTap value={html} onChange={() => {}} />
+          </CardContent>
+        </Card>
       </Grid>
       <Grid item xs={12}>
-        <Box
-          ref={ref}
-          sx={{
-            overflow: "auto",
-            width: "25dvw",
-            height: 100,
-            border: "1px red solid",
-          }}
-        >
-          <Box
-            sx={{
-              width: 150,
-            }}
-          ></Box>
-        </Box>
-        <hr />
-        <Box
-          sx={{
-            width: "25dvw",
-            height: 100,
-            border: "1px red solid",
-          }}
-        >
-          <ScrollView>
-            <Box
-              sx={{
-                width: 150,
-              }}
-            ></Box>
-          </ScrollView>
-        </Box>
-      </Grid>
-      <Grid item xs={12}>
-        <TipTap value={html} onChange={() => {}} />
-        <Divider />
-        <TipTap value={html} onChange={setHtml} />
-      </Grid>
-      <Grid item xs={12}>
-        <StyledForm>
-          <InputBase
-            value={chatInput}
-            onChange={(evt) => {
-              setChatInput(evt.target.value);
-            }}
-            onKeyDown={(evt) => {
-              if (evt.key !== "Enter") {
-                return;
-              }
-
-              if (evt.ctrlKey || evt.altKey || evt.shiftKey) {
-                setChatInput((prev) => prev.concat("\n"));
-                return;
-              }
-
-              evt.preventDefault();
-              chatInput.trim() && window.alert(chatInput.trim());
-            }}
-            fullWidth
-            multiline
-            maxRows={6}
-            placeholder="chat message here..."
-          />
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Box
-              sx={{
-                display: "flex",
-                gap(theme) {
-                  return theme.spacing(1);
-                },
-              }}
-            >
-              <IconButton
-                sx={{
-                  bgcolor(theme) {
-                    return theme.palette.action.selected;
-                  },
-                }}
-              >
-                <AddOutlined />
-              </IconButton>
-              <IconButton>
-                <AddReactionOutlined />
-              </IconButton>
-            </Box>
-            <IconButton
-              sx={{
-                marginInlineStart: "auto",
-              }}
-            >
-              <SendOutlined color="primary" />
-            </IconButton>
-          </Box>
-        </StyledForm>
+        <Card>
+          <CardHeader title="editable" />
+          <CardContent>
+            <TipTap value={html} onChange={setHtml} />
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
 }
 
 const bgImgHref = new URL(bgImg, import.meta.url).href;
-
-const StyledForm = styled("form")(({ theme }) => {
-  return {
-    border: "1px solid " + theme.palette.divider,
-    paddingInline: theme.spacing(3.5),
-    paddingBlock: theme.spacing(2),
-    borderRadius: theme.shape.borderRadius,
-  };
-});
