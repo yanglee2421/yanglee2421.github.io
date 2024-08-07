aa94b97af381b108f9d9168002e57bfbca5fb083
yanglee2421 <yanglee2421@gmail.com>
2024-08-07 13:22:02 +0800

    fix: show pending page when auth not ready

--- src/components/guard/AuthGuard.tsx
+++ src/components/guard/AuthGuard.tsx
@@ -1,9 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAuth } from "firebase/auth";
 import React from "react";
+import { app } from "@/api/firebase/app";
 import { useCurrentUser } from "@/hooks/store/useCurrentUser";
 import { NavigateToLogin } from "./NavigateToLogin";
 
 export function AuthGuard(props: React.PropsWithChildren) {
   const currentUser = useCurrentUser();
+
+  const query = useQuery({
+    queryKey: ["firebase/authStateReady"],
+    async queryFn() {
+      await getAuth(app).authStateReady();
+
+      return getAuth(app);
+    },
+  });
+
+  if (query.isPending) {
+    return <div>loading...</div>;
+  }
+
+  if (query.isError) {
+    return <div>{query.error.message}</div>;
+  }
 
   if (currentUser) {
     return props.children;
