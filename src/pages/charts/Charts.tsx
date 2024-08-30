f2e7a796c85f023bcfbaf9f2e556f5cda8236bcf
yanglee2421 <yanglee2421@gmail.com>
2024-08-31 01:46:45 +0800

    wip: remove recharts

--- src/pages/charts/Charts.tsx
+++ src/pages/charts/Charts.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactApexChart from "react-apexcharts";
 import { AnimateController } from "@/libs/AnimateController";
 import { Realtime } from "./Realtime";
-import { Recharts } from "./Recharts";
 
 export function Charts() {
   React.useEffect(() => {
@@ -31,54 +30,51 @@ export function Charts() {
   }, []);
 
   return (
-    <>
-      <ReactApexChart
-        options={{
-          chart: {
-            id: "realtime",
-            type: "line",
-            animations: {
-              easing: "linear",
-              dynamicAnimation: {
-                speed: 1000,
-              },
+    <ReactApexChart
+      options={{
+        chart: {
+          id: "realtime",
+          type: "line",
+          animations: {
+            easing: "linear",
+            dynamicAnimation: {
+              speed: 1000,
             },
-            toolbar: {
-              show: false,
-            },
-            zoom: { enabled: false },
           },
-          stroke: {
-            curve: "smooth",
-          },
-          xaxis: {
-            type: "datetime",
-            range: realtime.range,
-            labels: {
-              // show: false,
-            },
-            tooltip: { enabled: false },
-          },
-          yaxis: {
-            max: 100,
+          toolbar: {
             show: false,
-            stepSize: 25,
-            tooltip: { enabled: false },
           },
-          tooltip: {
-            enabled: false,
+          zoom: { enabled: false },
+        },
+        stroke: {
+          curve: "smooth",
+        },
+        xaxis: {
+          type: "datetime",
+          range: realtime.range,
+          labels: {
+            // show: false,
           },
-        }}
-        series={[
-          {
-            data: realtime.data.slice(),
-          },
-        ]}
-        type="line"
-        height={350}
-      />
-      <Recharts />
-    </>
+          tooltip: { enabled: false },
+        },
+        yaxis: {
+          max: 100,
+          show: false,
+          stepSize: 25,
+          tooltip: { enabled: false },
+        },
+        tooltip: {
+          enabled: false,
+        },
+      }}
+      series={[
+        {
+          data: realtime.data.slice(),
+        },
+      ]}
+      type="line"
+      height={350}
+    />
   );
 }
 
