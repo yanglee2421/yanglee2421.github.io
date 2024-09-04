ee3783799cf35798a09eb9e234a30ccf3f1ff238
yanglee2421 <yanglee2421@gmail.com>
2024-09-04 15:58:45 +0800

    feat: add icon & styling loading

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import forms from "@tailwindcss/forms";
 import { type Config } from "tailwindcss";
+import { addIconSelectors } from "@iconify/tailwind";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [forms()],
+  plugins: [forms(), addIconSelectors(["heroicons"])],
   darkMode: ["selector", ".dark"],
   corePlugins: {
     preflight: true,
