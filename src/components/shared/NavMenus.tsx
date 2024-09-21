873b89dc612c265d247e1ee3f17f53d6fc8f903b
yang <yanglee2421@gmail.com>
2024-09-21 23:34:33 +0800

    fix: slider not wroking on touch

--- src/components/shared/NavMenus.tsx
+++ src/components/shared/NavMenus.tsx
@@ -2,17 +2,19 @@ import { NavLink } from "react-router-dom";
 
 export function NavMenus() {
   return (
-    <nav className="flex space-x-4 border px-6 py-2 sm:justify-center">
-      {navList.map((item) => (
-        <NavLink
-          to={item.href}
-          key={item.href}
-          className="rounded-lg px-3 py-2 font-medium capitalize text-slate-700 hover:bg-slate-100 hover:text-slate-900 aria-[current=page]:text-blue-500"
-        >
-          {item.label}
-        </NavLink>
-      ))}
-    </nav>
+    <div className="overflow-auto">
+      <nav className="flex space-x-4 border px-6 py-2 sm:justify-center">
+        {navList.map((item) => (
+          <NavLink
+            to={item.href}
+            key={item.href}
+            className="rounded-lg px-3 py-2 font-medium capitalize text-slate-700 hover:bg-slate-100 hover:text-slate-900 aria-[current=page]:text-blue-500"
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </nav>
+    </div>
   );
 }
 
