03c1fd3c28f05171a491009b61efb4f15f3a8372
yanglee2421 <yanglee2421@gmail.com>
2024-09-24 13:18:13 +0800

    feat: add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,20 @@
-import forms from "@tailwindcss/forms";
 import { type Config } from "tailwindcss";
-import { addIconSelectors } from "@iconify/tailwind";
+import animate from "tailwindcss-animate";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [forms(), addIconSelectors(["heroicons"]), require("tailwindcss-animate")],
-  darkMode: ["selector", ".dark", "class"],
+  plugins: [animate],
+  darkMode: ["selector", ".dark"],
   corePlugins: {
     preflight: true,
   },
