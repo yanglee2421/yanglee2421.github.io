5274a1975b81152c0b28879de7673dd52a00f23f
yanglee2421 <yanglee2421@gmail.com>
2024-10-31 12:00:47 +0800

    chore: remove headless

--- src/pages/calendar/Calendar.tsx
+++ src/pages/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { HeadlessCalendar } from "@/components/headless/HeadlessCalendar";
+import { timeToCalendar } from "@/utils/timeToCalendar";
 import { Clock } from "./Clock";
 import {
   Card,
@@ -16,6 +16,7 @@ import { chunk } from "@yotulee/run";
 export function Calendar() {
   const [selectedTime, setSelectedTime] = React.useState(() => Date.now());
   const { i18n } = useTranslation();
+  const calendar = timeToCalendar(selectedTime).map((i) => new Date(i));
 
   return (
     <Card>
@@ -34,38 +35,32 @@ export function Calendar() {
           className="block w-full text-slate-700 focus:border-blue-500 focus:ring-blue-500"
         />
       </CardContent>
-      <HeadlessCalendar
-        selectedTime={selectedTime}
-        render={(props) => {
-          return (
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {props.data.slice(0, 7).map((date) => {
-                    const weekday = date.toLocaleString(i18n.language, {
-                      weekday: "short",
-                    });
+      <Table>
+        <TableHead>
+          <TableRow>
+            {calendar.slice(0, 7).map((date) => {
+              const weekday = date.toLocaleString(i18n.language, {
+                weekday: "short",
+              });
 
-                    return <TableCell key={weekday}>{weekday}</TableCell>;
-                  })}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {chunk(props.data, 7)
-                  .map((row, idx) => (
-                    <TableRow key={idx}>
-                      {row.map((cell) => (
-                        <TableCell key={cell.toLocaleDateString()}>
-                          {cell.getDate()}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          );
-        }}
-      />
+              return <TableCell key={weekday}>{weekday}</TableCell>;
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {chunk(calendar, 7)
+            .map((row, idx) => (
+              <TableRow key={idx}>
+                {row.map((cell) => (
+                  <TableCell key={cell.toLocaleDateString()}>
+                    {cell.getDate()}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      {timeToCalendar(selectedTime)}
     </Card>
   );
 }
---
+++ src/utils/timeToCalendar.tsx
@@ -0,0 +1,14 @@
+export function timeToCalendar(time: number) {
+  const monthStartTime = new Date(time).setDate(1);
+  const monthStartIndex = new Date(monthStartTime).getDay();
+  const calendarStartTime = monthStartTime - monthStartIndex * dayInterval;
+  const calendar = [];
+
+  for (let i = 0; i < 42; i++) {
+    calendar.push(calendarStartTime + i * dayInterval);
+  }
+
+  return calendar;
+}
+
+const dayInterval = 1000 * 60 * 60 * 24;
