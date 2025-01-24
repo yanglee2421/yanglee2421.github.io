21928b4f2ba37bd0e9c26a2ec128f9ab8fe1b15f
yang <yanglee2421@gmail.com>
2025-01-24 19:47:06 +0800

    style: sort out code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { QueryProvider } from "@/components/query";
 import { RouterUI } from "@/router/RouterUI";
+import React from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { useLocaleStore } from "@/hooks/store/useLocaleStore";
+
+const fallback = (
+  <Box
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      gap: 6,
+
+      position: "fixed",
+      inset: 0,
+      zIndex(theme) {
+        return theme.zIndex.modal;
+      },
+    }}
+  >
+    <CircularProgress />
+    <Typography>Loading...</Typography>
+  </Box>
+);
 
 export function App() {
+  const hasHydrated = React.useSyncExternalStore(
+    (onStoreChange) => useLocaleStore.persist.onFinishHydration(onStoreChange),
+    () => useLocaleStore.persist.hasHydrated(),
+    () => false
+  );
+
+  if (!hasHydrated) {
+    return fallback;
+  }
+
   return (
-    <QueryProvider>
+    <React.Suspense fallback={fallback}>
       <RouterUI />
-    </QueryProvider>
+    </React.Suspense>
   );
 }
