7ba633f5fc88f2cdacf220eecfe00e4bccad1c47
yanglee2421 <yanglee2421@gmail.com>
2025-04-15 11:44:02 +0800

    wip[app]: add dexie database

---
+++ src/lib/db.ts
@@ -0,0 +1,38 @@
+import Dexie, { type EntityTable } from "dexie";
+
+export type Completion = {
+  id: number;
+  name: string;
+};
+
+export type MessageStatus = "pending" | "success" | "error" | "loading";
+
+export type Message = {
+  id: number;
+  completionId: number; // Foreign key to the Completion table
+  question: string;
+  questionDate: string;
+  messages: Message[];
+  answer: string;
+  answerDate: null | string;
+  status: MessageStatus;
+  thumb: "up" | "down" | null;
+};
+
+export const db = new Dexie("FriendsDatabase") as Dexie & {
+  completions: EntityTable<
+    Completion,
+    "id" // primary key "id" (for the typings only)
+  >;
+  messages: EntityTable<
+    Message,
+    "id" // primary key "id" (for the typings only)
+  >;
+};
+
+// Schema declaration:
+db.version(1).stores({
+  completions: "++id, name", // primary key "id" (for the runtime!)
+  messages:
+    "++id, completionId, question, questionDate, answer, answerDate, status, thumb", // primary key "id" (for the runtime!)
+});
