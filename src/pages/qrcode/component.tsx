a9ad2ac54c3a4b83f0ff963d500889adc31fe448
yanglee2421 <yanglee2421@gmail.com>
2025-06-17 13:38:58 +0800

    feat[qrcode]: add read qrcode from image file

--- src/pages/qrcode/component.tsx
+++ src/pages/qrcode/component.tsx
@@ -9,6 +9,50 @@ import {
 import React from "react";
 import * as consts from "@/lib/constants";
 import { QrCode2Outlined } from "@mui/icons-material";
+import { readBarcodes } from "zxing-wasm";
+
+const PasteInput = () => {
+  const [pasteValue, setPasteValue] = React.useState("");
+  const [barcode, setBarcode] = React.useState("");
+
+  return (
+    <div
+      onDrag={(e) => {
+        const file = e.dataTransfer.files.item(0);
+        if (!file) return;
+        console.log(file);
+      }}
+    >
+      <img
+        src={pasteValue}
+        onLoad={(e) => {
+          URL.revokeObjectURL(e.currentTarget.src);
+        }}
+        alt=""
+      />
+      <p>{barcode}</p>
+      <input
+        type="text"
+        onPaste={async (e) => {
+          const file = e.clipboardData.files.item(0);
+          if (!file) return;
+          if (!file.type.startsWith("image/")) {
+            alert("Please paste an image file.");
+            return;
+          }
+          const url = URL.createObjectURL(file);
+          setPasteValue(url);
+
+          const data = await readBarcodes(file);
+          data.forEach((barcode) => {
+            if (!barcode.isValid) return;
+            setBarcode(barcode.text);
+          });
+        }}
+      />
+    </div>
+  );
+};
 
 export const Component = () => {
   const [qrValue, setQrValue] = React.useState(consts.GITHUB_URL);
@@ -59,6 +103,7 @@ export const Component = () => {
           </Grid>
         </Grid>
       </form>
+      <PasteInput />
     </Box>
   );
 };
