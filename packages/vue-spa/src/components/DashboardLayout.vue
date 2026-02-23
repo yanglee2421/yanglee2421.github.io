e5582fef3aada5293cf89f5e0a146a70c97c0ddf
yanglee2421 <yanglee2421@gmail.com>
2026-02-24 00:26:19 +0800

    chore: Add prettier plugin tailwindcss & remove unnecessary config.js

--- packages/vue-spa/src/components/DashboardLayout.vue
+++ packages/vue-spa/src/components/DashboardLayout.vue
@@ -2,36 +2,36 @@
 
 <template>
   <div
-    class="TeamsDark bg-[var(--colorNeutralBackground1)] text-[var(--colorNeutralForeground1)] min-h-screen"
+    class="TeamsDark min-h-screen bg-(--colorNeutralBackground1) text-(--colorNeutralForeground1)"
   >
     <header
-      class="box-border h-12 z-20 border-0 border-b border-solid border-[var(--colorNeutralStroke1)] p-3 flex justify-start items-center fixed inset-x-0 inset-bs-0"
+      class="fixed inset-x-0 inset-bs-0 z-20 box-border flex h-12 items-center justify-start border-0 border-b border-solid border-(--colorNeutralStroke1) p-3"
     >
       <h1 class="title3">header</h1>
     </header>
     <aside
-      class="fixed z-10 inset-s-0 inset-bs-0 pbs-12 md:w-64 w-full h-dvh md:block bg-[var(--colorNeutralBackground1)]"
+      class="fixed inset-s-0 inset-bs-0 z-10 h-dvh w-full bg-(--colorNeutralBackground1) pbs-12 md:block md:w-64"
     >
       <nav
-        class="overflow-auto h-full box-border border-solid border-[var(--colorNeutralStroke1)] border-0 border-e"
+        class="box-border h-full overflow-auto border-0 border-e border-solid border-(--colorNeutralStroke1)"
       >
-        <ul class="h-200 aside-nav__list">
+        <ul class="aside-nav__list h-200">
           <li
-            class="p-3 border-b border-0 border-solid border-[var(--colorNeutralStroke1)]"
+            class="border-0 border-b border-solid border-(--colorNeutralStroke1) p-3"
           >
             <RouterLink
               :to="{ name: 'home' }"
-              class="text-indigo-500 hover:text-indigo-600 [&.router-link-active]:text-indigo-700 hover:underline"
+              class="text-indigo-500 hover:text-indigo-600 hover:underline [&.router-link-active]:text-indigo-700"
             >
               <span class="body2">Home</span>
             </RouterLink>
           </li>
           <li
-            class="p-3 border-0 border-b border-solid border-[var(--colorNeutralStroke1)]"
+            class="border-0 border-b border-solid border-(--colorNeutralStroke1) p-3"
           >
             <RouterLink
               :to="{ name: 'about' }"
-              class="text-indigo-500 hover:text-indigo-600 [&.router-link-active]:text-indigo-700 hover:underline"
+              class="text-indigo-500 hover:text-indigo-600 hover:underline [&.router-link-active]:text-indigo-700"
             >
               <span class="body2">About</span>
             </RouterLink>
@@ -39,15 +39,15 @@
           <li
             v-for="value in 20"
             :key="value"
-            class="p-3 border-0 border-b border-solid border-[var(--colorNeutralStroke1)]"
+            class="border-0 border-b border-solid border-(--colorNeutralStroke1) p-3"
           >
             <span class="body2">{{ value }}</span>
           </li>
         </ul>
       </nav>
     </aside>
-    <main class="md:ps-64 pbs-12 min-h-dvh flex flex-col box-border">
-      <div class="p-[var(--spacingHorizontalL)] flex-grow">
+    <main class="box-border flex min-h-dvh flex-col pbs-12 md:ps-64">
+      <div class="grow p-(--spacingHorizontalL)">
         <slot></slot>
       </div>
       <footer class="p-3 text-center">
