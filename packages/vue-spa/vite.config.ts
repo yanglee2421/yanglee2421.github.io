17bfd43d140531d5a1f04e3100b001c111d00006
yanglee2421 <yanglee2421@gmail.com>
2026-02-23 23:11:03 +0800

    refactor[vue-spa]: Migrate unocss to tailwindcss

--- packages/vue-spa/vite.config.ts
+++ packages/vue-spa/vite.config.ts
@@ -2,13 +2,13 @@ import path from "node:path";
 import url from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import UnoCSS from "unocss/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), tailwindcss()],
 
   resolve: {
     alias: {
