866cfe7fdb690cb84b0e8126e66094c5a72b29f7
yang <yanglee2421@gmail.com>
2026-03-08 20:00:51 +0800

    style: Sort out code

--- packages/vue-spa/src/router/routes.ts
+++ packages/vue-spa/src/router/routes.ts
@@ -2,64 +2,66 @@ import { useLocalStorage } from "@vueuse/core";
 import type { RouteRecordRaw } from "vue-router";
 import { localeService } from "../shared/locale";
 
-export const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    children: [
-      {
-        name: "index",
-        path: "",
-        redirect: (to) => {
-          const storedLocaleRef = useLocalStorage(
-            "locale",
-            localeService.getLocale(),
-          );
-          localeService.setLocale(storedLocaleRef.value);
-          storedLocaleRef.value = localeService.getLocale();
-          return localeService.resolvePathname(to.path);
+export const createRoutes = (): RouteRecordRaw[] => {
+  return [
+    {
+      path: "/",
+      children: [
+        {
+          name: "index",
+          path: "",
+          redirect: (to) => {
+            const storedLocaleRef = useLocalStorage(
+              "locale",
+              localeService.getLocale(),
+            );
+            localeService.setLocale(storedLocaleRef.value);
+            storedLocaleRef.value = localeService.getLocale();
+            return localeService.resolvePathname(to.path);
+          },
         },
-      },
-      {
-        path: ":lang",
-        component: () => import("./LangGuard.vue"),
-        beforeEnter: (to, _, next) => {
-          const lang = to.params.lang as string;
-          const storedLocaleRef = useLocalStorage(
-            "locale",
-            localeService.getLocale(),
-          );
-          localeService.setLocale(storedLocaleRef.value);
-          localeService.setLocale(lang);
-          const locale = localeService.getLocale();
-          storedLocaleRef.value = locale;
+        {
+          path: ":lang",
+          component: () => import("./LangGuard.vue"),
+          beforeEnter: (to, _, next) => {
+            const lang = to.params.lang as string;
+            const storedLocaleRef = useLocalStorage(
+              "locale",
+              localeService.getLocale(),
+            );
+            localeService.setLocale(storedLocaleRef.value);
+            localeService.setLocale(lang);
+            const locale = localeService.getLocale();
+            storedLocaleRef.value = locale;
 
-          if (locale === lang) {
-            next();
-            return;
-          }
+            if (locale === lang) {
+              next();
+              return;
+            }
 
-          next({
-            path: localeService.resolvePathname(to.path),
-          });
+            next({
+              path: localeService.resolvePathname(to.path),
+            });
+          },
+          children: [
+            {
+              name: "not-found",
+              path: ":pathMatch(.*)*",
+              component: () => import("../pages/NotFoundPage.vue"),
+            },
+            {
+              name: "home",
+              path: "",
+              component: () => import("../pages/HomePage.vue"),
+            },
+            {
+              name: "about",
+              path: "about",
+              component: () => import("../pages/AboutPage.vue"),
+            },
+          ],
         },
-        children: [
-          {
-            name: "not-found",
-            path: ":pathMatch(.*)*",
-            component: () => import("../pages/NotFoundPage.vue"),
-          },
-          {
-            name: "home",
-            path: "",
-            component: () => import("../pages/HomePage.vue"),
-          },
-          {
-            name: "about",
-            path: "about",
-            component: () => import("../pages/AboutPage.vue"),
-          },
-        ],
-      },
-    ],
-  },
-];
+      ],
+    },
+  ];
+};
