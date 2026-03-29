f7f77d27a3e2fbec6d74c863ff570edb42dcdbcf
yanglee2421 <yanglee2421@gmail.com>
2026-03-30 01:32:53 +0800

    ai change

--- packages/react-spa/src/shared/ServiceApp.ts
+++ packages/react-spa/src/shared/ServiceApp.ts
@@ -1,19 +1,26 @@
-class ServiceAppContext<TContext extends {}> {
-  private context: TContext;
+type Prettierify<T> = {
+  [K in keyof T]: T[K];
+} & {};
 
-  constructor(context: TContext) {
-    this.context = context;
-  }
+type ServiceAppContext<TContext extends {} = {}> = Readonly<TContext> & {
+  set<TNewContext extends {}>(
+    context: TNewContext,
+  ): ServiceAppContext<Prettierify<TContext & TNewContext>>;
+};
 
-  set<TNewContext extends {}>(context: TNewContext) {
-    this.context = { ...this.context, ...context };
-
-    return this as unknown as ServiceAppContext<TContext & TNewContext>;
+const ServiceAppContext: {
+  new <TContext extends {} = {}>(
+    context: TContext,
+  ): ServiceAppContext<TContext>;
+} = class {
+  constructor(context: Record<string, unknown>) {
+    Object.assign(this, context);
   }
-  get(): TContext {
-    return this.context;
+  set(context: Record<string, unknown>) {
+    Object.assign(this, context);
+    return this;
   }
-}
+} as never;
 
 export class ServiceApp<TAppContext extends {}> {
   private context: ServiceAppContext<TAppContext>;
@@ -29,6 +36,10 @@ export class ServiceApp<TAppContext extends {}> {
       ? ServiceApp<Context>
       : ServiceApp<TAppContext>;
   }
+
+  get() {
+    return this.context as unknown as Readonly<Prettierify<TAppContext>>;
+  }
 }
 
 const app = new ServiceApp(new ServiceAppContext({}))
@@ -36,9 +47,9 @@ const app = new ServiceApp(new ServiceAppContext({}))
     return c.set({ name: "yanglee2421.github.io" });
   })
   .use((c) => {
-    console.log(c);
+    console.log(c.name); // 直接访问，有类型提示
 
     return c.set({ version: "1.0.0" });
   });
 
-void app;
+void app.get().version;
