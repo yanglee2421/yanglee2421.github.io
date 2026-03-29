88a1dad3d14f08d8e5450cb03865d0778c23fea6
yang <yanglee2421@gmail.com>
2026-03-29 14:37:21 +0800

    wip[print]: Working on print 501

--- packages/vue-spa/src/pages/AboutPage.vue
+++ packages/vue-spa/src/pages/AboutPage.vue
@@ -15,61 +15,70 @@ const date = new Date().toLocaleDateString("zh-CN", {
     <button class="btn btn-primary" @click="handlePrint">click me</button>
   </DashboardLayout>
   <Teleport to="body">
-    <article id="print" class="hidden text-center text-[10pt]">
-      <section class="h-[297mm] w-[210mm] border border-dashed p-10">
-        <div class="text-[12pt]">
-          <p class="text-end">辆货统-501</p>
-          <h1 class="text-[16pt] font-semibold">
-            铁路货车轮轴多通道超声波自动探伤系统日常性能校验记录表
-          </h1>
-          <p>&nbsp;</p>
-        </div>
-        <table class="w-full table-fixed" cellspacing="0">
+    <article id="print" class="hidden">
+      <section data-a4>
+        <p class="text-end text-[12pt]">辆货统-501</p>
+        <h1 data-caption>
+          铁路货车轮轴多通道超声波自动探伤系统日常性能校验记录表
+        </h1>
+        <p>&nbsp;</p>
+        <table cellspacing="0">
+          <colgroup>
+            <col class="w-[80pt]" />
+            <col />
+            <col class="w-[80pt]" />
+            <col />
+          </colgroup>
+          <thead>
+            <tr class="text-[12pt]">
+              <th>单位名称</th>
+              <td class="underline underline-offset-3">
+                江岸车辆段武南轮厂轮轴车间
+              </td>
+              <th class="">检验时间</th>
+              <td class="underline underline-offset-3">
+                {{ date }}
+              </td>
+            </tr>
+          </thead>
+        </table>
+        <table cellspacing="0">
           <thead>
             <tr>
-              <td colspan="11" class="border-0!">
-                <table class="w-full table-fixed">
-                  <colgroup>
-                    <col class="w-[80pt]" />
-                    <col />
-                    <col class="w-[80pt]" />
-                    <col />
-                  </colgroup>
-                  <thead>
-                    <tr class="text-[12pt]">
-                      <th class="border-0!">单位名称</th>
-                      <td class="border-0! underline underline-offset-3">
-                        江岸车辆段武南轮厂轮轴车间
-                      </td>
-                      <th class="border-0!">检验时间</th>
-                      <td class="border-0! underline underline-offset-3">
-                        {{ date }}
-                      </td>
+              <td colspan="11">
+                <table cellspacing="0">
+                  <tbody>
+                    <tr class="*:text-[12pt]">
+                      <th>设备型号</th>
+                      <td></td>
+                      <th>设备编号</th>
+                      <td></td>
+                      <th>实物试块型号</th>
+                      <td></td>
                     </tr>
-                  </thead>
+                  </tbody>
                 </table>
               </td>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td colspan="11" class="">
-                <table class="w-full table-fixed">
-                  <tbody>
-                    <tr class="*:text-[12pt]">
-                      <th class="border-y-0! border-l-0!">设备型号</th>
-                      <td class="border-y-0!"></td>
-                      <th class="border-y-0!">设备编号</th>
-                      <td class="border-y-0!"></td>
-                      <th class="border-y-0!">实物试块型号</th>
-                      <td class="border-y-0! border-r-0!"></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-            <tr>
-              <th rowspan="19">R D 2 试 样 轴 轮 座 人 工 缺 陷 编 号</th>
+              <th rowspan="20">
+                R
+                <br />
+                D <br />
+                2 <br />试 <br />
+                样 <br />
+                轴 <br />
+                轮 <br />
+                座 <br />
+                人 <br />
+                工 <br />
+                缺 <br />
+                陷 <br />
+                编 <br />
+                号
+              </th>
               <th colspan="5">左轮座探头晶片编号及灵敏度</th>
               <th colspan="5">右轮座探头晶片编号及灵敏度</th>
             </tr>
@@ -125,74 +134,348 @@ const date = new Date().toLocaleDateString("zh-CN", {
               <td></td>
               <td></td>
             </tr>
-            <tr v-for="(item, index) in 13" :key="index">
-              <td colspan="2">{{ item }}</td>
+            <tr v-for="(item, index) in 13" :key="index" class="*:text-[12pt]">
+              <th colspan="2">{{ item }}</th>
               <td></td>
               <td></td>
               <td></td>
-              <td colspan="2">{{ item }}</td>
+              <th colspan="2">{{ item }}</th>
               <td></td>
               <td></td>
               <td></td>
             </tr>
-            <tr>
-              <td>ss</td>
+            <tr v-if="true">
+              <td colspan="5">
+                <table cellspacing="0">
+                  <tbody>
+                    <tr>
+                      <th rowspan="2">左</th>
+                      <th rowspan="2">通道编号</th>
+                      <th rowspan="2">拆射角度</th>
+                      <th colspan="3">灵敏度(dB)</th>
+                      <th colspan="3">缺陷编号</th>
+                    </tr>
+                    <tr>
+                      <th>
+                        校验
+                        <br />
+                        (80%)
+                      </th>
+                      <th>补偿</th>
+                      <th>探伤</th>
+                      <th>1</th>
+                      <th>2</th>
+                      <th>3</th>
+                    </tr>
+                    <tr>
+                      <th rowspan="3" class="text-[12pt]">
+                        轴
+                        <br />
+                        颈
+                      </th>
+                      <th>CT</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td colspan="3"></td>
+                    </tr>
+                    <tr>
+                      <th>A1</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <th>A2</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+              <td colspan="5">
+                <table cellspacing="0">
+                  <tbody>
+                    <tr>
+                      <th rowspan="2">右</th>
+                      <th rowspan="2">通道编号</th>
+                      <th rowspan="2">拆射角度</th>
+                      <th colspan="3">灵敏度(dB)</th>
+                      <th colspan="3">缺陷编号</th>
+                    </tr>
+                    <tr>
+                      <th>
+                        校验
+                        <br />
+                        (80%)
+                      </th>
+                      <th>补偿</th>
+                      <th>探伤</th>
+                      <th>1</th>
+                      <th>2</th>
+                      <th>3</th>
+                    </tr>
+                    <tr>
+                      <th rowspan="3" class="text-[12pt]">
+                        轴
+                        <br />
+                        颈
+                      </th>
+                      <th>CT</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td colspan="3"></td>
+                    </tr>
+                    <tr>
+                      <th>A1</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <th>A2</th>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
               <td colspan="11">
-                <table class="w-full table-fixed text-[12pt]">
-                  <tr class="*:border-y-0! *:p-1.5">
-                    <th rowspan="2" class="border-l-0!">签字签章</th>
-                    <th>探伤工</th>
-                    <td></td>
-                    <th>探伤工长</th>
-                    <td class="border-r-0!"></td>
-                  </tr>
-                  <tr class="*:border-b-0! *:p-1.5">
-                    <th>质检员</th>
-                    <td></td>
-                    <th>验收员</th>
-                    <td class="border-r-0!"></td>
-                  </tr>
+                <table class="text-[12pt]" cellspacing="0">
+                  <tbody>
+                    <tr>
+                      <th rowspan="2">签字签章</th>
+                      <th>探伤工</th>
+                      <td></td>
+                      <th>探伤工长</th>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <th>质检员</th>
+                      <td></td>
+                      <th>验收员</th>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <th>备注</th>
+                      <td colspan="4"></td>
+                    </tr>
+                  </tbody>
                 </table>
               </td>
             </tr>
           </tfoot>
         </table>
+        <p class="mt-6 text-end text-[12pt]">第 1 页</p>
       </section>
-      <section>
-        <h1></h1>
+      <section data-a4>
+        <p class="text-end text-[12pt]">辆货统-501</p>
+        <h1 data-caption="">
+          铁路货车轮轴超声波自动探伤系统日常性能校验记录表（第2页）
+        </h1>
+        <p>&nbsp;</p>
+        <table cellspacing="0" class="text-[12pt]">
+          <colgroup>
+            <col class="w-[80pt]" />
+            <col />
+            <col class="w-[80pt]" />
+            <col />
+          </colgroup>
+          <thead>
+            <tr class="text-[12pt]">
+              <th>样板轮型号</th>
+              <td class="underline underline-offset-3">
+                江岸车辆段武南轮厂轮轴车间
+              </td>
+              <th class="">校验时间</th>
+              <td class="underline underline-offset-3">
+                {{ date }}
+              </td>
+            </tr>
+          </thead>
+        </table>
+        <table cellspacing="0">
+          <tbody>
+            <tr>
+              <td class="p-0! text-[12pt]">左轴颈根部扫描图</td>
+              <td class="p-0! text-[12pt]">右轴颈根部扫描图</td>
+            </tr>
+            <tr>
+              <td class="h-[150pt] p-0!"></td>
+              <td class="h-[150pt] p-0!"></td>
+            </tr>
+            <tr>
+              <td class="p-0! text-[12pt]">左轴颈根部扫描图</td>
+              <td class="p-0! text-[12pt]">右轴颈根部扫描图</td>
+            </tr>
+            <tr>
+              <td class="h-[150pt] p-0!"></td>
+              <td class="h-[150pt] p-0!"></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="p-0! text-[12pt]">左穿透扫描图</td>
+            </tr>
+            <tr>
+              <td colspan="2" class="h-[150pt] p-0!"></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="p-0! text-[12pt]">右穿透扫描图</td>
+            </tr>
+            <tr>
+              <td colspan="2" class="h-[150pt] p-0!"></td>
+            </tr>
+          </tbody>
+        </table>
+        <p class="mt-6 text-end text-[12pt]">第 2 页</p>
       </section>
     </article>
   </Teleport>
 </template>
 
 <style>
+[data-a4] {
+  box-sizing: border-box;
+  width: 210mm;
+  height: 297mm;
+
+  padding: 16mm 14mm;
+  margin: 0;
+
+  border: 1px dashed black;
+
+  break-after: page;
+
+  font-family: "SimSun", "宋体", serif;
+  font-size: 10pt;
+
+  text-align: center;
+
+  &::before,
+  &::after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: separate;
+    table-layout: fixed;
+  }
+}
+
+[data-caption] {
+  font-size: 16pt;
+  font-weight: 600;
+}
+
+table {
+  > thead {
+    > tr {
+      > td,
+      > th {
+        > table {
+          > tbody {
+            > tr {
+              > td,
+              > th {
+                padding: 3pt;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  > tbody {
+    > tr {
+      > td,
+      > th {
+        padding: 2pt;
+
+        border: solid black;
+        border-width: 1pt 1pt 0 0;
+
+        &:has(> table) {
+          padding: 0;
+          border: 0;
+        }
+
+        > table {
+          td,
+          th {
+            padding: 2pt;
+          }
+        }
+      }
+    }
+  }
+  > tfoot {
+    > tr {
+      > td,
+      > th {
+        > table {
+          > tbody {
+            > tr {
+              > td,
+              > th {
+                padding: 3pt;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+table:has(tbody) {
+  border: solid black;
+  border-width: 0 0 1pt 1pt;
+}
+
 @page {
-  size: A4;
+  size: A4 portrait;
   margin: 0;
 }
 
-section {
-  break-after: page;
-}
-
 @media print {
-  button {
-    display: none;
-  }
   #app {
     display: none;
   }
   #print {
     display: block;
   }
+  [data-a4] {
+    /* border: 0; */
+  }
 }
-
-td,
-th {
-  border: 1pt solid black;
+td > table {
+  border: 0;
 }
 </style>
